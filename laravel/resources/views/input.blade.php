fd239aeda51b63c181687fe53879b5dd661331d0
NS <re13brb26l28@hotmail.com>
2021-01-23 20:52:21 +0900

    頻度フォームメニュー

--- laravel/resources/views/input.blade.php
+++ laravel/resources/views/input.blade.php
@@ -4,10 +4,24 @@
 @include('header')
   <div class="top-page">
     <div class="top_title">
-      <h1>お薬登録画面</h1>
-      <h2>お薬の名前</h2>
-      <h2>頻度</h2>
-      <h2>飲む時間</h2>
+      <form action="{{url('/inputs')}}" method="POST">
+        <h1>お薬登録画面</h1>
+        <h2>お薬の名前 <input type="text" name="medicine_name"></h2>
+        <h2>頻度 
+          <select name='sometime'>
+            @for ($count=1; $count<=7; $count++)
+              @if ($count==1)
+                <option value='{{$count}}'>毎日</option>
+              @elseif ($count<=6)
+                <option value='{{$count}}'>{{$count}}日に一度</option>
+              @else
+                <option value='{{$count}}'>一週間に一度</option>
+              @endif
+            @endfor            
+          </select></h2>
+        <h2>飲む時間 <input type="text" name="time_to"></h2>
+        
+      </form>
     </div>
   </div>
 
