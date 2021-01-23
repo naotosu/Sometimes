7937a0ed944044bc816f79bc5ad5d7414cfb3539
NS <re13brb26l28@hotmail.com>
2021-01-23 23:22:14 +0900

    時間登録-仮作成

--- laravel/resources/views/input.blade.php
+++ laravel/resources/views/input.blade.php
@@ -8,7 +8,7 @@
         <h1>お薬登録画面</h1>
         <h2>お薬の名前 <input type="text" name="medicine_name"></h2>
         <h2>頻度 
-          <select name='sometime'>
+          <select name='sometime' input type="date">
             @for ($count=1; $count<=7; $count++)
               @if ($count==1)
                 <option value='{{$count}}'>毎日</option>
@@ -19,8 +19,12 @@
               @endif
             @endfor            
           </select></h2>
-        <h2>飲む時間 <input type="text" name="time_to"></h2>
-        
+        <h2>飲む時間 
+        <select name="time_to" input type="date">
+          @for ($i = 0; $i < 96; $i++)
+            <option>{{date("H:i", strtotime("+". $i * 30 ." minute"))}}</option>
+          @endfor
+        </h2>
       </form>
     </div>
   </div>
