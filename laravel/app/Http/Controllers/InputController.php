fbcb19654eb3ea146d97da8529c0dede916a0a24
NS <re13brb26l28@hotmail.com>
2021-01-24 11:14:35 +0900

    時間仮仕様

--- laravel/app/Http/Controllers/InputController.php
+++ laravel/app/Http/Controllers/InputController.php
@@ -10,4 +10,19 @@ class InputController extends Controller
     {
         return view('input');
     }
+
+    // public function input(Request $request)
+    // {
+    //     $medicine_name = $request->input('medicine_name');
+    //     $count = $request->input('count');
+    //     $time_to = $request->input('$i');
+
+    //     try {
+    //         $sometimes = Sometime::SearchBySometime()->get();
+
+    //         $ = new $sometime
+    //     }
+
+    //     return view('input');
+    // }
 }
--- laravel/resources/views/input.blade.php
+++ laravel/resources/views/input.blade.php
@@ -6,7 +6,7 @@
     <div class="top_title">
       <form action="{{url('/inputs')}}" method="POST">
         <h1>お薬登録画面</h1>
-        <h2>お薬の名前 <input type="text" name="medicine_name"></h2>
+        <h2>お薬の名前 <input type="text" name="medicine_name" value="薬の名前"></h2>
         <h2>頻度 
           <select name='sometime' input type="date">
             @for ($count=1; $count<=7; $count++)
@@ -19,12 +19,20 @@
               @endif
             @endfor            
           </select></h2>
-        <h2>飲む時間 
-        <select name="time_to" input type="date">
-          @for ($i = 0; $i < 96; $i++)
-            <option>{{date("H:i", strtotime("+". $i * 30 ." minute"))}}</option>
-          @endfor
-        </h2>
+        <h2>飲む時間 <!-- TODO 0：00〜30分刻みで選択出来る様にしたい　-->
+          <select name="time_to" input type="date">
+            <option value='6:00'>6:00</option>
+            <option value='8:00'>8:00</option>
+            <option value='12:00'>12:00</option>
+            <option value='13:00'>13:00</option>
+            <option value='18:00'>18:00</option>
+            <option value='20:00'>20:00</option>
+            <!-- @for ($i = 0; $i < 96; $i++)
+              <option>value='{{$i}}' date("H:i", strtotime("+". $i * 30 ." minute"))}}</option>
+            @endfor -->
+
+          </h2>
+        <h2><input type="submit" value="登録"></h2>
       </form>
     </div>
   </div>
