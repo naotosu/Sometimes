e41332df7a919fcbf33b80f04832ce5a3987e34f
NS <re13brb26l28@hotmail.com>
2021-01-28 20:35:21 +0900

    css調整

--- laravel/resources/views/input.blade.php
+++ laravel/resources/views/input.blade.php
@@ -6,10 +6,12 @@
     <div class="top_title">
       <form action="{{url('/input_data')}}" method="POST">
         {{ csrf_field() }}
-        <h1>お薬登録画面</h1>
-        {{$today = \Carbon\Carbon::today()}}
-        <h2>お薬の名前 <input type="text" name="medicine_name" value="薬の名前"></h2>
-        <h2>頻度 
+        <h2>お薬登録画面</h2>
+        <div class="input_data">
+          {{$today = \Carbon\Carbon::today()}}
+        </div>
+        <h3>お薬の名前 <input type="text" name="medicine_name" value="薬の名前"></h3>
+        <h3>頻度 
           <select name='sometime' input type="date">
             @for ($count=1; $count<=7; $count++)
               @if ($count==1)
@@ -20,8 +22,8 @@
                 <option value='{{$count}}'>一週間に一度</option>
               @endif
             @endfor            
-          </select></h2>
-        <h2>飲む時間 <!-- TODO 0：00〜30分刻みで選択出来る様にしたい　-->
+          </select></h3>
+        <h3>飲む時間 <!-- TODO 0：00〜30分刻みで選択出来る様にしたい　-->
           <select name="time_to" input type="date">
             <option value='{{$today->addHours(6)}}'>6:00</option>
             <option value='{{$today->addHours(2)}}'>8:00</option>
@@ -29,10 +31,10 @@
             <option value='{{$today->addHours(1)}}'>13:00</option>
             <option value='{{$today->addHours(5)}}'>18:00</option>
             <option value='{{$today->addHours(2)}}'>20:00</option>
-          </h2>
+          </h3>
         <h2><input type="submit" value="登録"></h2>
       </form>
-      <table border="1">
+      <table border="1" align="center">
         <tr>
           <th>ユーザーID</th>
           <th>お薬の名前</th>
