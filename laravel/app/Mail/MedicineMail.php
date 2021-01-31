3e582845c7eb52c98e1fdf62805b4842eefa1c24
NS <re13brb26l28@hotmail.com>
2021-01-31 10:12:02 +0900

    メール文章作成メソッド修正

--- laravel/app/Mail/MedicineMail.php
+++ laravel/app/Mail/MedicineMail.php
@@ -16,7 +16,7 @@ class MedicineMail extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user_name, $medicine)
     {
         $this->user_name = $user_name;
         $this->medicine = $medicine;
@@ -29,9 +29,8 @@ class MedicineMail extends Mailable
      */
     public function build()
     {
-        //TODO Laravel8でのviewへの参照を調査して修正する
-        return $this->view(''emails.notification_mail'');
-                        ->subject('お薬の時間です')　
+        return $this->view('emails.notification_mail')
+                        ->subject('お薬の時間です')
                         ->with([
                             'user_name' => $this->user_name,
                             'medicine' => $this->medicine,
